58e8020245f0589b9f1c48030e205c3ca6d7f909
Ubuntu <azureuser@container-maker.3ndjwfy5zpyuhazidbomuvgz2g.ax.internal.cloudapp.net>
2022-03-24 13:24:24 +0000

    add api-server cluster ip service

--- build.ps1
+++ build.ps1
@@ -2,3 +2,4 @@ if ((Get-AzContext) -eq $null) {
     Connect-AzAccount -UseDeviceAuthentication
 }
 Connect-AzContainerRegistry -Name indcr
+Import-AzAksCredential -ResourceGroupName ind -Name indaks -Force
